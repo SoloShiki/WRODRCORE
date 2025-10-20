ea7b10eb519f5aa010a0c8595c86c26d789a140a
Edwin Marte <60978455+EdwinMarteZorrilla@users.noreply.github.com>
2025-10-20 16:43:33 -0400

    Update pwm_servo_controller_3_servos.py

--- 11. FireFighter/Servo_test/pwm_servo_controller_3_servos.py
+++ 11. FireFighter/Servo_test/pwm_servo_controller_3_servos.py
@@ -9,7 +9,7 @@ class PWMServoController(Node):
     def __init__(self):
         super().__init__('pwm_servo_controller')
 
-        # Control 3 servos with IDs 1, 2, and 3
+        # Servo IDs to control
         self.servo_ids = [1, 2, 3]
 
         # Declare parameters
@@ -25,30 +25,31 @@ class PWMServoController(Node):
         self.timer_interval = self.get_parameter('timer_interval').get_parameter_value().double_value
 
         # Publisher
-        self.publisher = self.create_publisher(SetPWMServoState, '/ros_robot_controller/pwm_servo/set_state', 10)
+        self.publisher = self.create_publisher(SetPWMServoState,
+                                               '/ros_robot_controller/pwm_servo/set_state',
+                                               10)
 
-        # Initialize servo state
+        # Initialize servo states
         self.positions = {sid: self.initial_position for sid in self.servo_ids}
         self.directions = {sid: 1 for sid in self.servo_ids}
 
-        # Timer for motion updates
+        # Timer
         self.timer = self.create_timer(self.timer_interval, self.timer_callback)
 
         # Send initial positions
         self.send_servo_positions()
 
     def send_servo_positions(self):
-        msg = SetPWMServoState()
-        servo_state = PWMServoState()
-
-        # Combine all IDs and positions into one PWMServoState
-        servo_state.id = list(self.positions.keys())
-        servo_state.position = list(self.positions.values())
-        servo_state.offset = [self.offset] * len(self.servo_ids)
-
-        msg.state.append(servo_state)
-
-        self.publisher.publish(msg)
+        # Send one message per servo
+        for sid in self.servo_ids:
+            msg = SetPWMServoState()
+            servo_state = PWMServoState()
+            servo_state.id = [sid]
+            servo_state.position = [self.positions[sid]]
+            servo_state.offset = [self.offset]
+            msg.state.append(servo_state)
+            self.publisher.publish(msg)
+        
         pos_text = ', '.join(f'ID {sid}: {pos}' for sid, pos in self.positions.items())
         self.get_logger().info(f'Sent positions -> {pos_text}')
 
@@ -59,7 +60,6 @@ class PWMServoController(Node):
             if self.positions[sid] >= 2500 or self.positions[sid] <= 500:
                 self.directions[sid] *= -1
                 self.positions[sid] = max(500, min(self.positions[sid], 2500))
-
         self.send_servo_positions()
 
 def main(args=None):
